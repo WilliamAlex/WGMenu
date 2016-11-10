68718b34c04b87cd0915398772cbbd83d3d667d7
Uncle Alex <1509159596@qq.com>
2016-11-10 17:53:42 +0800

    Initial Commit

---
+++ WGMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGMenu
+//
+//  Created by Alex William on 2016/11/10.
+//  Copyright © 2016年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGMenu
+//
+//  Created by Alex William on 2016/11/10.
+//  Copyright © 2016年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
